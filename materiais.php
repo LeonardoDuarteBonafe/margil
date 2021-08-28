11e8b497f00ba03e00522c11c3051810ac4c7d70
LeonardoDuarteBonafe <leo_nardo136@hotmail.com>
2021-08-28 09:13:53 -0300

    Adicionando galeria a todos os materiais

--- materiais.php
+++ materiais.php
@@ -44,64 +44,112 @@
                 <a href="Images/Materiais/Amarelo-Maracuja/close.png" data-lightbox="galeriaAmareloMaracuja" data-title="Close"></a>
             </div>
             <div class="box" id="Amarelo-Ornamental-G">
-                <img src="Images/Materiais/Amarelo-Ornamental-G/Amarelo-Ornamental-G.png">
-                <p>Amarelo Ornamental G</p>
+                <a href="Images/Materiais/Amarelo-Ornamental-G/Amarelo-Ornamental-G.png" data-lightbox="galeriaAmareloOrnamentalG" data-title="Amarelo Ornamental G">
+                    <img src="Images/Materiais/Amarelo-Ornamental-G/Amarelo-Ornamental-G.png">
+                    <p>Amarelo Ornamental G</p>
+                </a>
+                <a href="Images/Materiais/Amarelo-Ornamental-G/close.png" data-lightbox="galeriaAmareloOrnamentalG" data-title="Close"></a>
             </div>
             <div class="box" id="Amarelo-Ornamental-VL">
-                <img src="Images/Materiais/Amarelo-Ornamental-VL/Amarelo-Ornamental-VL.png">
-                <p>Amarelo Ornamental VL</p>
+                <a href="Images/Materiais/Amarelo-Ornamental-VL/Amarelo-Ornamental-VL.png" data-lightbox="galeriaAmareloOrnamentalVL" data-title="Amarelo Ornamental VL">
+                    <img src="Images/Materiais/Amarelo-Ornamental-VL/Amarelo-Ornamental-VL.png">
+                    <p>Amarelo Ornamental VL</p>
+                </a>
+                <a href="Images/Materiais/Amarelo-Ornamental-VL/close.png" data-lightbox="galeriaAmareloOrnamentalVL" data-title="Close"></a>
             </div>
             <div class="box" id="Aqua-Marine-White">
-                <img src="Images/Materiais/Aqua-Marine-White/Aqua-Marine-White.png">
-                <p>Aqua Marine White</p>
+                <a href="Images/Materiais/Aqua-Marine-White/Aqua-Marine-White.png" data-lightbox="galeriaAquaMarineWhite" data-title="Aqua Marine White">
+                    <img src="Images/Materiais/Aqua-Marine-White/Aqua-Marine-White.png">
+                    <p>Aqua Marine White</p>
+                </a>
+                <a href="Images/Materiais/Aqua-Marine-White/close.png" data-lightbox="galeriaAquaMarineWhite" data-title="Close"></a>
+                <a href="Images/Materiais/Aqua-Marine-White/ambiente.png" data-lightbox="galeriaAquaMarineWhite" data-title="Ambiente"></a>
             </div>
             <div class="box" id="Branco-Dallas">
-                <img src="Images/Materiais/Branco-Dallas/Branco-Dallas.png">
-                <p>Branco Dallas</p>
+                <a href="Images/Materiais/Branco-Dallas/Branco-Dallas.png" data-lightbox="galeriaBrancoDallas" data-title="Branco Dallas">
+                    <img src="Images/Materiais/Branco-Dallas/Branco-Dallas.png">
+                    <p>Branco Dallas</p>
+                </a>
+                <a href="Images/Materiais/Branco-Dallas/close.png" data-lightbox="galeriaBrancoDallas" data-title="Close"></a>
             </div>
             <div class="box" id="Branco-Itaunas">
-                <img src="Images/Materiais/Branco-Itaunas/Branco-Itaunas.png">
-                <p>Branco Itaunas</p>
+                <a href="Images/Materiais/Branco-Itaunas/Branco-Itaunas.png" data-lightbox="galeriaBrancoItaunas" data-title="Branco Itaunas">
+                    <img src="Images/Materiais/Branco-Itaunas/Branco-Itaunas.png">
+                    <p>Branco Itaunas</p>
+                </a>
+                <a href="Images/Materiais/Branco-Itaunas/close.png" data-lightbox="galeriaBrancoItaunas" data-title="Close"></a>
             </div>
             <div class="box" id="Branco-Siena">
-                <img src="Images/Materiais/Branco-Siena/Branco-Siena.png">
-                <p>Branco Siena</p>
+                <a href="Images/Materiais/Branco-Siena/Branco-Siena.png" data-lightbox="galeriaBrancoSiena" data-title="Branco Siena">
+                    <img src="Images/Materiais/Branco-Siena/Branco-Siena.png">
+                    <p>Branco Siena</p>
+                </a>
+                <a href="Images/Materiais/Branco-Siena/close.png" data-lightbox="galeriaBrancoSiena" data-title="Close"></a>
             </div>
             <div class="box" id="Cavalese-White">
-                <img src="Images/Materiais/Cavalese-White/Cavalese-White.png">
-                <p>Cavalese White</p>
+                <a href="Images/Materiais/Cavalese-White/Cavalese-White.png" data-lightbox="galeriaCavaleseWhite" data-title="Cavalese White">
+                    <img src="Images/Materiais/Cavalese-White/Cavalese-White.png">
+                    <p>Cavalese White</p>
+                </a>
+                <a href="Images/Materiais/Cavalese-White/close.png" data-lightbox="galeriaCavaleseWhite" data-title="Close"></a>
             </div>
             <div class="box" id="Cinza-Andorinha">
-                <img src="Images/Materiais/Cinza-Andorinha/Cinza-Andorinha.png">
-                <p>Cinza Andorinha</p>
+                <a href="Images/Materiais/Cinza-Andorinha/Cinza-Andorinha.png" data-lightbox="galeriaCinzaAndorinha" data-title="Cinza Andorinha">
+                    <img src="Images/Materiais/Cinza-Andorinha/Cinza-Andorinha.png">
+                    <p>Cinza Andorinha</p>
+                </a>
+                <a href="Images/Materiais/Cinza-Andorinha/close.png" data-lightbox="galeriaCinzaAndorinha" data-title="Close"></a>
             </div>
             <div class="box" id="Cinza-Corumba">
-                <img src="Images/Materiais/Cinza-Corumba/Cinza-Corumba.png">
-                <p>Cinza Corumba</p>
+                <a href="Images/Materiais/Cinza-Corumba/Cinza-Corumba.png" data-lightbox="galeriaCinzaCorumba" data-title="Cinza Corumba">
+                    <img src="Images/Materiais/Cinza-Corumba/Cinza-Corumba.png">
+                    <p>Cinza Corumba</p>
+                </a>
+                <a href="Images/Materiais/Cinza-Corumba/close.png" data-lightbox="galeriaCinzaCorumba" data-title="Close"></a>
+            </div>
+            <div class="box" id="Cinza-Corumbazinho">
+                <a href="Images/Materiais/Cinza-Corumbazinho/Cinza-Corumbazinho.png" data-lightbox="galeriaCinzaCorumbazinho" data-title="Cinza Corumbazinho">
+                    <img src="Images/Materiais/Cinza-Corumbazinho/Cinza-Corumbazinho.png">
+                    <p>Cinza Corumbazinho</p>
+                </a>
+                <a href="Images/Materiais/Cinza-Corumbazinho/close.png" data-lightbox="galeriaCinzaCorumbazinho" data-title="Close"></a>
             </div>
             <div class="box" id="">
-                <img src="Images/Materiais/Cinza-Corumbazinho/Cinza-Corumbazinho.png">
-                <p>Cinza Corumbazinho</p>
-            </div>
-            <div class="box" id="">
-                <img src="Images/Materiais/Preto-Sao-Rafael/Preto-Sao-Rafael.png">
-                <p>Preto Sao Rafael</p>
+                <a href="Images/Materiais/Preto-Sao-Rafael/Preto-Sao-Rafael.png" data-lightbox="galeriaPretoSaoRafael" data-title="Preto Sao Rafael">
+                    <img src="Images/Materiais/Preto-Sao-Rafael/Preto-Sao-Rafael.png">
+                    <p>Preto Sao Rafael</p>
+                </a>
+                <a href="Images/Materiais/Preto-Sao-Rafael/close.png" data-lightbox="galeriaPretoSaoRafael" data-title="Close"></a>
+                <a href="Images/Materiais/Preto-Sao-Rafael/ambiente.png" data-lightbox="galeriaPretoSaoRafael" data-title="Ambiente"></a>
             </div>
             <div class="box" id="Preto-Via-Lactea">
-                <img src="Images/Materiais/Preto-Via-Lactea/Preto-Via-Lactea.png">
-                <p>Preto Via Lactea</p>
+                <a href="Images/Materiais/Preto-Via-Lactea/Preto-Via-Lactea.png" data-lightbox="galeriaPretoViaLactea" data-title="Preto Via Lactea">
+                    <img src="Images/Materiais/Preto-Via-Lactea/Preto-Via-Lactea.png">
+                    <p>Preto Via Lactea</p>
+                </a>
+                <a href="Images/Materiais/Preto-Via-Lactea/close.png" data-lightbox="galeriaPretoViaLactea" data-title="Close"></a>
             </div>
             <div class="box" id="Travertino">
-                <img src="Images/Materiais/Travertino/Travertino.png">
-                <p>Travertino</p>
+                <a href="Images/Materiais/Travertino/Travertino.png" data-lightbox="galeriaTravertino" data-title="Travertino">
+                    <img src="Images/Materiais/Travertino/Travertino.png">
+                    <p>Travertino</p>
+                </a>
+                <a href="Images/Materiais/Travertino/close.png" data-lightbox="galeriaTravertino" data-title="Close"></a>
+                <a href="Images/Materiais/Travertino/ambiente.png" data-lightbox="galeriaTravertino" data-title="Ambiente"></a>
             </div>
             <div class="box" id="Verde-Ubatuba">
-                <img src="Images/Materiais/Verde-Ubatuba/Verde-Ubatuba.png">
-                <p>Verde Ubatuba</p>
+                <a href="Images/Materiais/Verde-Ubatuba/Verde-Ubatuba.png" data-lightbox="galeriaVerdeUbatuba" data-title="Verde Ubatuba">
+                    <img src="Images/Materiais/Verde-Ubatuba/Verde-Ubatuba.png">
+                    <p>Verde Ubatuba</p>
+                </a>
+                <a href="Images/Materiais/Verde-Ubatuba/close.png" data-lightbox="galeriaVerdeUbatuba" data-title="Close"></a>
             </div>
             <div class="box" id="White-Persa">
-                <img src="Images/Materiais/White-Persa/White-Persa.png">
-                <p>White Persa</p>
+                <a href="Images/Materiais/White-Persa/White-Persa.png" data-lightbox="galeriaWhitePersa" data-title="White Persa">
+                    <img src="Images/Materiais/White-Persa/White-Persa.png">
+                    <p>White Persa</p>
+                </a>
+                <a href="Images/Materiais/White-Persa/close.png" data-lightbox="galeriaWhitePersa" data-title="Close"></a>
             </div>
 
         </div>
